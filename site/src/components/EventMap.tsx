ce030d7cb040373d22c240535b7f4cec62ebeb72
Eoin O'Brien <eoobrien@microsoft.com>
2025-09-19 18:01:30 +0100

    switch back to osm tiles

--- site/src/components/EventMap.tsx
+++ site/src/components/EventMap.tsx
@@ -139,8 +139,8 @@ export default function EventMap({
       className="w-full lg:h-dvh h-[80dvh]"
     >
       <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       <MarkerClusterGroup ref={clusterRef} chunkedLoading>
